99628d114a59ec31b07b492fa856a93b2434428d
Paul Marfleet <paul.marfleet@gmail.com>
2015-07-12 14:47:07 +0100

    Revert "Added query + handler to get currency"
    This reverts commit d7741593b7aa2e6eb0e0087e8a03238f9d040018.

--- Pricing.Console/Program.cs
+++ Pricing.Console/Program.cs
@@ -1,6 +1,4 @@
-﻿using Pricing.Core.Queries;
-using Pricing.Core.QueryHandlers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,33 +10,6 @@ namespace Pricing.Console
     {
         static void Main(string[] args)
         {
-            var currencyCode = args[0];
-
-            var getCurrencyQueryHandler = new GetCurrencyQueryHandler();
-            var currency = getCurrencyQueryHandler.Handle(new GetCurrencyQuery {
-                Code = currencyCode
-            });
-            
-            var getTourDeparturesQueryHandler = new GetTourDeparturesQueryHandler();
-            var tourDepartures = getTourDeparturesQueryHandler.Handle(new GetTourDeparturesQuery());
-
-            var getExchangeRatesQueryHandler = new GetExchangeRatesQueryHandler();
-            var exchangeRates = getExchangeRatesQueryHandler.Handle(new GetExchangeRatesQuery {
-                To = currency
-            });
-
-            var getMarkupRateQueryHandler = new GetMarkupRateQueryHandler();
-
-            foreach (var tourDeparture in tourDepartures)
-            {
-                var exchangeRate = exchangeRates.First(rate => rate.From == tourDeparture.LocalCost.Currency);
-                var convertedCost = exchangeRate.Convert(tourDeparture.LocalCost);
-                var markupRate = getMarkupRateQueryHandler.Handle(new GetMarkupRateQuery
-                {
-                    Currency = currency,
-                    From = tourDeparture.F
-                });
-            }
         }
     }
 }

d7741593b7aa2e6eb0e0087e8a03238f9d040018
Paul Marfleet <paul.marfleet@gmail.com>
2015-07-12 14:44:22 +0100

    Added query + handler to get currency

---
+++ Pricing.Core/Queries/GetCurrencyQuery.cs
@@ -0,0 +1,14 @@
+﻿using Pricing.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pricing.Core.Queries
+{
+    public class GetCurrencyQuery : IQuery<Currency>
+    {
+        public string Code { get; set; }
+    }
+}
